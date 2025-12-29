0865e502fd8b8ac4223a573f4d7f55c7312b462d
HoHux <171756268@qq.com>
2025-12-29 10:44:37 +0800

    feat(projects): init pancake

---
+++ src/layouts/modules/global-footer/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'GlobalFooter'
+});
+</script>
+
+<template>
+  <DarkModeContainer class="h-full flex-center">
+    <a href="https://github.com/aihohu/pancake-web/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">
+      Copyright MIT © 2025 Pancake Admin
+    </a>
+  </DarkModeContainer>
+</template>
+
+<style scoped></style>

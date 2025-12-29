ed9d12b0334959e45ddbec8020b142cbb19c30b2
HoHux <171756268@qq.com>
2025-12-29 20:36:02 +0800

    refactor(projects): i18n

--- src/locales/langs/en-us/page/index.ts
+++
@@ -1,9 +0,0 @@
-import home from './home';
-import login from './login';
-
-const page: App.I18n.Schema['page'] = {
-  login,
-  home
-};
-
-export default page;
--- src/typings/locales/page.d.ts
+++
@@ -1,7 +0,0 @@
-/** 预先定义命名空间 */
-declare namespace I18nPage {
-  // 必须定义一个基础接口，方便后面合并
-  interface AllPages {
-    _base?: unknown;
-  }
-}
